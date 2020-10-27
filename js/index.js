8c3cad9ab01df1b43741cc452121f5283b4b48e4
Katie Olson <katieolson@katies-air.lan>
2020-10-27 14:19:48 -0600

    changed logo color on mouseover & mouseleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+// change logo color
+ const logo = document.querySelector ("h1.logo-heading");
+
+ logo.addEventListener("mouseover", function (event){
+     event.target.style.color ="red";
+ });
+ logo.addEventListener("mouseleave", function (event){
+     event.target.style.color ="black";
+ });
+
